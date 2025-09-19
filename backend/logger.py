6a201c602d30425cdd417c8d406b6596ec9ed8db
Krishna Gelra <krishnagelra18@gmail.com>
2025-09-19 07:48:34 +0530

    Adding Backend

---
+++ backend/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+def setup_logger(name="AI-Healthcare-Assistant"):
+    logger=logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler=logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter=logging.Formatter("[%(asctime)s] [%(levelname)s] --- [%(message)s]")
+    console_handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(console_handler)
+
+    return logger
+
+
+logger=setup_logger()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("What can this AI Health Bot do for you?")
+
+
+if __name__ == "__main__":
+    main()
